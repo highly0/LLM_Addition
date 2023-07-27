b06cc3663150af3c4f67a39bd61165d08bef927c
highly0 <le@nlp2.skoltech.ru>
2023-07-27 23:26:49 +0300

    mlm and casual lm baseline

--- src/dataprep.py
+++ src/dataprep.py
@@ -7,12 +7,13 @@ from transformers import (
 class AdditionDataset(torch.utils.data.Dataset):
     def __init__(self, file_path, tokenizer: AutoTokenizer):
         with open(file_path, encoding="utf-8") as file:
-            texts = [line.rstrip() for line in file]
-        
-        self.encodings = tokenizer(texts, truncation=True, padding=True)
+            self.texts = [line.rstrip() for line in file]
+
+        self.encodings = tokenizer(self.texts, truncation=True, padding=True)
 
     def __len__(self):
         return len(self.encodings.input_ids)
 
     def __getitem__(self, idx):
-        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item_dict = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        return item_dict
--- src/main.py
+++ src/main.py
@@ -8,6 +8,13 @@ from transformers import (
     DataCollatorForLanguageModeling,
     TrainingArguments,
     Trainer,
+    GPT2LMHeadModel,
+    AutoConfig,
+    XLMRobertaForMaskedLM, 
+    RobertaForMaskedLM,
+    XLMRobertaConfig,
+    RobertaConfig,
+    GPT2Config,
 )
 from dataprep import AdditionDataset
 
@@ -23,7 +30,7 @@ parser.add_argument(
     "--eval_data_file",
     default="/workspace/LLM_Addition/data/eval/test_2_add.txt",
     type=str,
-    help="The input training data file (a text file).",
+    help="The input evaluation data file (a text file).",
 )
 parser.add_argument(
     "--output_dir",
@@ -34,27 +41,34 @@ parser.add_argument(
 parser.add_argument(
     "--model_name",
     type=str,
-    default="gpt2",
+    default="roberta-large",
     help="model type",
 )
 parser.add_argument(
     "--train_batch_size",
     type=int,
     default=16,
-    help="model type",
+    help="training batch size",
+)
+parser.add_argument(
+    "--train_type",
+    type=str,
+    default='scratch',
+    choices=['finetuned', 'scratch'],
+    help="training batch size",
 )
 parser.add_argument(
     "--eval_batch_size",
     type=int,
     default=32,
-    help="model type",
+    help="eval batch size",
 )
 
 
 MODEL_CLASSES = {
-    "xlm-roberta-large": AutoModelForMaskedLM,
-    "distilroberta-base": AutoModelForMaskedLM,
-    "gpt2": AutoModelForCausalLM
+    "xlm-roberta-large": (AutoModelForMaskedLM, XLMRobertaConfig, XLMRobertaForMaskedLM),
+    "roberta-large": (AutoModelForMaskedLM, RobertaConfig, RobertaForMaskedLM),
+    "gpt2": (AutoModelForCausalLM, GPT2Config, GPT2LMHeadModel), 
 }
 
 
@@ -62,26 +76,39 @@ if __name__ == "__main__":
     args = parser.parse_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
-    auto_model = MODEL_CLASSES[args.model_name]
-    model = auto_model.from_pretrained(args.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name, return_special_tokens_mask=True)
+    if args.train_type == "scratch": # training model from scratch
+        _, model_config, head_model = MODEL_CLASSES[args.model_name]
+        config = model_config.from_pretrained(
+            args.model_name,
+            vocab_size=len(tokenizer),
+            n_ctx=128,
+            bos_token_id=tokenizer.bos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+        model = head_model(config)
+    else: # fine-tuning
+        auto_model, _, _ = MODEL_CLASSES[args.model_name]
+        model = auto_model.from_pretrained(args.model_name)
 
     tokenizer.pad_token = tokenizer.eos_token
-    is_mlm = False if args.model_name == 'gpt2' else True
+    IS_MLM = False if args.model_name == "gpt2" else True
     data_collator = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer, mlm_probability=0.15, mlm=is_mlm
+        tokenizer=tokenizer, mlm_probability=0.15, mlm=IS_MLM
     )
 
     train_dataset = AdditionDataset(file_path=args.train_data_file, tokenizer=tokenizer)
     eval_dataset = AdditionDataset(file_path=args.eval_data_file, tokenizer=tokenizer)
-    train_type = args.train_data_file.split('/')[-1].split('.')[0]
-    
+    train_type = args.train_data_file.split("/")[-1].split(".")[0]
+
     training_args = TrainingArguments(
-        output_dir=f"{args.output_dir}/{train_type}/{args.model_name}/results",
-        logging_dir=f"{args.output_dir}/{train_type}/{args.model_name}/logs",
+        output_dir=f"{args.output_dir}/{train_type}/{args.model_name}_{args.train_type}/results",
+        logging_dir=f"{args.output_dir}/{train_type}/{args.model_name}_{args.train_type}/logs",
         num_train_epochs=5,
         learning_rate=2e-5,
         weight_decay=0.01,
+        warmup_steps=100,
+        lr_scheduler_type="cosine",
         per_device_train_batch_size=args.train_batch_size,  # batch size per device during training
         per_device_eval_batch_size=args.eval_batch_size,
         load_best_model_at_end=True,  # load the best model when finished training (default metric is loss)
@@ -101,3 +128,12 @@ if __name__ == "__main__":
     trainer.train()
     eval_results = trainer.evaluate()
     print(f"Perplexity: {math.exp(eval_results['eval_loss']):.2f}")
+    with open(
+        f"{args.output_dir}/{train_type}/{args.model_name}_{args.train_type}/evaluation.txt",
+        "w+",
+        encoding="utf-8",
+    ) as file:
+        file.write("trainer.evaluate() results:\n")
+        for k, v in eval_results.items():
+            file.write(f"{k}: {v}\n")
+        file.write(f"Final eval perplexity: {math.exp(eval_results['eval_loss']):.2f}\n")
